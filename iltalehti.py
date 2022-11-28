d845a7a657916acb7ab7f9197d11bc41e1dbd5f8
AntiEmulation <33380124+AntiEmulation@users.noreply.github.com>
2022-11-29 01:54:18 +0200

    Fix il-reader

--- iltalehti.py
+++ iltalehti.py
@@ -4,7 +4,7 @@ import time
 import codecs
 import sys
 
-json_start_mark = "id=\"alma-content-router-slot-ALMA_DATALAYER_SENDER\"></div></div></div><div><script>window.App="
+json_start_mark = "<script>window.App="
 json_end_mark = "</script><script nomodule="
 
 class color:
@@ -29,42 +29,41 @@ def print_purple(text):
 	print(color.PURPLE + text + color.END)
 
 def parse_json(json_str):
-    structure = json.loads(json_str)
-    article_id = ""
-    for k in structure['state']['articles']:
-       	article_id = k
-       	break
-
-    items = structure['state']['articles'][article_id]["items"]
-    for x in items["body"]:
-       	if (x["type"] == "paragraph"):
-        	print("\n")
-
-        	for e in x["items"]:
-        		if (e["type"] == "text"):
-        			print(e["text"])
-        		if (e["type"] == "bold" or e["type"] == "italic"):
-        			for n in e["items"]:
-        				if (n["type"] == "text"):
-        					if (e["type"] == "bold"):
-        						print_yellow(n["text"])
-        					else:
-        						print_purple(n["text"])
-
-        if (x["type"] == "image"):
-        	print_cyan("\nIMAGE: " + x["urls"]["default"])
-
+	structure = json.loads(json_str)
+	article_id = ""
+	for k in structure['state']['articles']:
+		article_id = k
+		break
+	
+	items = structure['state']['articles'][article_id]["items"]
+	for x in items["body"]:
+		if (x["type"] == "paragraph"):
+			print("\n")
+			
+			for e in x["items"]:
+				if (e["type"] == "text"):
+					print(e["text"])
+				if (e["type"] == "bold" or e["type"] == "italic"):
+					for n in e["items"]:
+						if (n["type"] == "text"):
+							if (e["type"] == "bold"):
+								print_yellow(n["text"])
+							else:
+								print_purple(n["text"])
+		if (x["type"] == "image"):
+			print_cyan("\nIMAGE: " + x["urls"]["default"])
 
 def load_url(url):
 	req = requests.get(url)
 	page_src = req.text
 	json_start = page_src.split(json_start_mark,1)[1]
 	json_str = json_start.split(json_end_mark,1)[0]
-	return json_str
+	final_json_str = json_str.replace("undefined", "null")
+	return final_json_str
 
 if (len(sys.argv) == 2):
 	json_str = load_url(sys.argv[1])
-	if (len(json_str) > 64):
+	if (len(json_str) > 0):
 		parse_json(json_str)
 
 
